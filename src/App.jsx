a17f0b60c049751a6bb988fb1e7a0fbfa7055280
Melina Escalante <meliescalantee@gmail.com>
2024-10-25 10:36:41 -0300

    use of states and render of list with conditionals

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,65 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css';
-import Card from './Card';
-import User from './User';
-import { useState, useEffect } from 'react';
+import "./App.css";
+import Card from "./Card";
+import User from "./User";
+import { useState, useEffect } from "react";
 function App() {
-  const [sneakers, setSneakers] = useState([])
+  const [sneakers, setSneakers] = useState([]);
   useEffect(() => {
     const getSneakers = async () => {
-      const endPoint = 'http://127.0.0.1:3000/api/sneakers'
-      const resp = await fetch(endPoint)
-      const sneakers = await resp
-      setSneakers(sneakers.data)
-    }
-    getSneakers()
-  }, [])
+      const endPoint = "http://127.0.0.1:3000/api/sneakers";
+      const resp = await fetch(endPoint);
+      const sneakers = await resp.json();
+      setSneakers(sneakers.data);
+      console.log(sneakers);
+    };
 
-  const title = <h1 className='font-medium text-3xl mt-10'>React Aplicaciones Híbridas</h1>
-  let clase = 2
-  const arrayUsers = [{ name: 'Juan', email: 'juan@gmail.com', age: 19 }, { name: 'Sofia', email: 'sofia@gmail.com', age: 24 }, { name: 'Lourdes', email: 'lourdes@gmail.com', age: 28 }, { name: 'Juan Rodriguez', email: 'juanr@gmail.com', age: 25 }]
-  const userData = { name: 'Juan', email: 'juan@gmail.com', age: 19 }
+    getSneakers();
+  }, []);
+
+  let [logueado, setLogueado] =useState(false);
+  const title = (
+    <h1 className="font-medium text-3xl mt-10">React Aplicaciones Híbridas</h1>
+  );
+  let clase = 2;
+  const login= () =>{
+    setLogueado(true)
+  }
+  const logout= () =>{
+    setLogueado(false)
+  }
   return (
     <div>
       {title}
-      <p className='m-2 mb-4'>Clase {clase} de React</p>
-      <img src="/react.jpg" alt="Imagen de react" className='w-2/5 mx-auto' />
-      <Card texto="Melina Escalante" edad={19} />
+
+      {logueado ? (
+        <>
+          <h2 className="text-2xl m-4 text-cyan-800 mb-6" >Bienvenido</h2>
+          <a  onClick={logout} className="text-xl px-2 py-1 rounded-lg text-white bg-cyan-600 cursor-pointer">Cerrar sesion</a>
+          <div className=" mt-4 flex flex-wrap items-stretch    ">
+            {sneakers.map((sneaker) => (
+              <div className="w-2/6">
+                <Card
+                  key={sneaker.id}
+                  name={sneaker.name}
+                  age={sneaker.description}
+                  email={sneaker.price}
+                ></Card>
+              </div>
+            ))}
+          </div>
+        </>
+      ) : (
+        <div className=" m-4">
+
+        <a  onClick={login} className="text-xl rounded-lg text-white px-2 py-1 bg-cyan-600 cursor-pointer">Iniciar sesion</a>
+        </div>
+      )}
+      {/* <p className='m-2 mb-4'>Clase {clase} de React</p> */}
+      {/* <img src="/react.jpg" alt="Imagen de react" className='w-2/5 mx-auto' /> */}
+      <User texto="Melina Escalante" edad={19} />
       {/* Mapeamos array */}
-      {
-        arrayUsers.map(user=>(
-      <User name={user.name} age={user.age}  email={user.email}></User>
-      ))
-  }
-      {/* <User name={userData.name} age={userData.age} email={userData.email}></User> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/User.jsx
+++ src/User.jsx
@@ -1,14 +1,9 @@
 import React from "react";
 
-function User({name, email, age}){
-  //Forma larga de destructurizar
-    // const name= props.name
-    // const email= props.email
-    // const age= props.age
-return(<div className="p-2 m-2 border rounded-lg border-blue-900">
-    <h4 className="font-medium text-lg ">{name}</h4>
-    <p>Email {email}</p>
-    <p>Edad {age}</p>
+function User(props){
+return(<div className="p-2 m-2 border rounded-lg border-cyan-400">
+    <h4>{props.texto}</h4>
+    <p>Edad {props.edad}</p>
 </div>)
 }
 export default User
