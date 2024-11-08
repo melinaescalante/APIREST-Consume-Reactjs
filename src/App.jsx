b403886005d53fbcda24ff74833ec950dfef0f90
Melina Escalante <meliescalantee@gmail.com>
2024-11-08 10:43:48 -0300

    Registro completado, vinculado a db

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,25 @@ import Contact from "./views/ConctactView";
 import Details from "./views/DetailsView";
 import NotFound from "./views/NotFoundView";
 import ApiRestView from "./views/ApiView";
+import SingUpView from "./views/SingUpView";
 function App() {
 
 
   const title = (
-    <h1 className="font-medium text-3xl mt-10">Tienda de zapatillas</h1>
+    <h1 className="font-medium text-3xl mt-5">Tienda de zapatillas</h1>
   );
 
   return (
     <div>
       {title}
       <nav>
-        <ul className="flex gap-6 text-cyan-600 font-medium">
+        <ul className="flex gap-6 text-cyan-600 font-medium mb-4 mt-8">
           <li><NavLink to='/'>Home</NavLink></li>
           <li><NavLink to='/login'>Login</NavLink></li>
           <li><NavLink to='/contact'>Contacto</NavLink></li>
           <li><NavLink to='/details'>Detalle</NavLink></li>
           <li><NavLink to='/api'>API</NavLink></li>
+          <li><NavLink to='/singup'>Registro</NavLink></li>
 
         </ul>
       </nav>
@@ -33,6 +35,7 @@ function App() {
         <Route path="/contact" element={<Contact></Contact>}></Route>
         <Route path="/details/:id" element={<Details></Details>}></Route>
         <Route path="/api" element={<ApiRestView></ApiRestView>}></Route>
+        <Route path="/singup" element={<SingUpView></SingUpView>}></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
      
--- src/views/ApiView.jsx
+++ src/views/ApiView.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import RouteContainer from "../components/RouteContainer";
 const ApiRestView = () => {
-    const routes = [
+    const routesUsers = [
         {
             name: 'GET',
             route: 'http://127.0.0.1:3000/api/users',
@@ -38,6 +38,47 @@ const ApiRestView = () => {
             color: 'bg-red-700'
         },
     ]
+    const routesSneakers = [
+        {
+            name: 'GET',
+            route: 'http://127.0.0.1:3000/api/sneakers',
+            color: 'bg-blue-700'
+        },
+        {
+            name: 'GET by id',
+            route: 'http://127.0.0.1:3000/api/sneakers/:id',
+            color: 'bg-blue-700'
+        },
+        {
+            name: 'GET by model',
+            route: 'http://127.0.0.1:3000/api/sneakers/model/:model',
+            color: 'bg-blue-700'
+        },{
+            name: 'GET by brand',
+            route: 'http://127.0.0.1:3000/api/sneakers/brand/:brand',
+            color: 'bg-blue-700'
+        },
+        {
+            name: 'POST',
+            route: 'http://127.0.0.1:3000/api/sneakers',
+            color: 'bg-green-700'
+        },
+        {
+            name: 'POST login',
+            route: 'http://127.0.0.1:3000/api/sneakers/login',
+            color: 'bg-green-700'
+        },
+        {
+            name: 'PUT',
+            route: 'http://127.0.0.1:3000/api/sneakers/:id',
+            color: 'bg-yellow-700'
+        },
+        {
+            name: 'DELETE',
+            route: 'http://127.0.0.1:3000/api/sneakers/:id',
+            color: 'bg-red-700'
+        },
+    ]
     return (
         <>
             <header class="p-4 bg-primary bg-gradient text-white d-flex justify-content-center align-items-center">
@@ -46,7 +87,16 @@ const ApiRestView = () => {
             </header>
             <RouteContainer name="Usuarios" key={RouteContainer.id}>
 
-                {routes.map((route) => (
+                {routesUsers.map((route) => (
+                    <li className=" list-none p-2 ms-0 mb-3 border-b w-full border-b-slate-200" key={route.name}>
+                        <span className={`py-1 px-2 ms-2 me-2 rounded-lg text-xs text-white font-medium  ${route.color}`} >{route.name}</span>
+                        {route.route}
+                    </li>
+                ))}
+            </RouteContainer>
+            <RouteContainer name="Sneakers" key={RouteContainer.id}>
+
+                {routesSneakers.map((route) => (
                     <li className=" list-none p-2 ms-0 mb-3 border-b w-full border-b-slate-200" key={route.name}>
                         <span className={`py-1 px-2 ms-2 me-2 rounded-lg text-xs text-white font-medium  ${route.color}`} >{route.name}</span>
                         {route.route}
